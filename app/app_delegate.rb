a3588b46f59966b1a1b761552e397992022ae682
Socjopata@imac <socjopata@gmail.com>
2012-09-05 12:15:52 +0200

    keyandvisible window, roller controller encapsulated in UINavigation Controller, first spec

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,18 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible # <- 'this window will receive taps, shakes and you name it'
+    
+    #initiate controller here. initWithNibName thing comes from the fact that typically, this is used to load a controller from a NIB file.
+    #NIBs are created using Xcode's Interface Builder as a way of visually constructing views.
+    #we are passing a nil twice because we are not using Interface Builder for our stuff.
+    @dice_roller = RollerController.alloc.initWithNibName(nil, bundle: nil)
+    #... and put it into standard iOS container
+    @navigation_controller = UINavigationController.alloc.initWithRootViewController(@dice_roller) 
+    
+    #UIWindow have a rootViewController property for displaying controllers. #Now it's time to set it up:
+    @window.rootViewController = @navigation_controller #the @window takes the UIViewController instance(our dice roller) and adjusts IT'S view size to the window. Neat
+    
     true
   end
 end
